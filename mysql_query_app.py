6280c731e3df534b5646399a39d5d07896bc94ff
Roberts, C <131574764+roberts-data-intell@users.noreply.github.com>
2023-10-25 00:52:38 -0500

    Add files via upload

---
+++ mysql_query_app.py
@@ -0,0 +1,116 @@
+
+from flask import Flask, render_template, Response, logging
+from sqlalchemy import create_engine, text
+from sqlalchemy.exc import SQLAlchemyError
+import pdfkit
+import os
+
+app = Flask(__name__)
+
+# Connect to MySQL Database with connection pooling
+DATABASE_URL = "mysql+mysqlconnector://root:T9r50wRgf44v@localhost/Sales_Project"
+engine = create_engine(DATABASE_URL, pool_size=10, max_overflow=20)
+
+# A utility function to run queries and fetch data
+def fetch_data(query):
+    try:
+        with engine.connect() as conn:
+            result = conn.execute(text(query))
+            data = result.fetchall()
+    except SQLAlchemyError as e:
+        app.logger.error("MySQL error: %s", e)
+        data = []
+    return data
+
+@app.route("/")
+def index():
+    # Top 10 Query 1 top ten products and their marketing budget and the profit
+    query1 = """SELECT `product_type`, SUM(`marketing`) as `totalmarketing`, SUM(`profit`) as `totalprofit`
+                           FROM `business_data`
+                            GROUP BY `product_type`
+                             ORDER BY `totalmarketing` DESC
+                              LIMIT 10;"""  
+    data1 = fetch_data(query1)
+    # Market - total profit and marketing expenses
+    query2 = """SELECT market, SUM(profit) as totalprofit, SUM(marketing) as totalmarketing
+                           FROM business_data
+                            GROUP BY market;"""
+    data2 = fetch_data(query2)
+    # Correlation Between Total Expenses and Profit in Each Market Size
+    query3 = """SELECT `market_size`,(COUNT(*) * SUM(`profit` * `total_expenses`) - SUM(`profit`) * SUM(`total_expenses`)) / 
+                           (SQRT(COUNT(*) * SUM(`profit` * `profit`) - SUM(`profit`) * SUM(`profit`)) * SQRT(COUNT(*) * SUM(`total_expenses` * `total_expenses`) - SUM(`total_expenses`) * SUM(`total_expenses`)))
+                            AS Correlation
+                             FROM `business_data`
+                              GROUP BY `market_size` HAVING COUNT(*) * SUM(`profit` * `profit`) - SUM(`profit`) * SUM(`profit`) > 0 AND 
+                               COUNT(*) * SUM(`total_expenses` * `total_expenses`) - SUM(`total_expenses`) * SUM(`total_expenses`) > 0;
+                                 """  
+    data3 = fetch_data(query3)
+    # Product Types That Have Seen Sales Growth Over Time and how much
+    query4 = """SELECT `product_type`, (MAX(sales) - MIN(sales)) AS SalesGrowth
+                            FROM (SELECT `product_type`, Date, SUM(sales) as sales
+                             FROM business_data
+                             GROUP BY `product_type`, Date)
+                              as SubQuery7
+                               GROUP BY `product_type` HAVING COUNT(sales) > 1 AND MAX(sales) > MIN(sales);
+                                """
+    data4 = fetch_data(query4)
+    # States Where Actual COGS Are Different From 
+        # Budget COGS and show by how much is the difference
+    query5 = """SELECT state, ABS(actual_cogs - budget_cogs) AS difference
+                           FROM (SELECT state, SUM(cogs) as actual_cogs, SUM(budget_cogs) as budget_cogs
+                            FROM business_data
+                             GROUP BY state) 
+                              as subquery6
+                               WHERE actual_cogs != budget_cogs;"""  
+    data5 = fetch_data(query5)
+    # Average Inventory for Each Product Type in Each State
+    query6 = """SELECT state, product_type, ROUND(AVG(inventory), 2) as avg_inventory
+                           FROM business_data
+                            GROUP BY state, product_type;"""
+    data6 = fetch_data(query6)
+    # States with Highest Sales
+    query7 = """SELECT state, MAX(sales) as MaxSales
+                           FROM (SELECT state, market, SUM(sales) as sales
+                            FROM business_data
+                             GROUP BY state, market) as sub_query_3
+                              GROUP BY state
+                               ORDER BY MaxSales DESC
+                                LIMIT 5;"""  
+    data7 = fetch_data(query7)
+    # Highest selling product in each market and what is the profit and Total Expenses
+    query8 = """SELECT market, MAX(product) as product,  
+                           MAX(sales) as HighestSales,
+                            SUM(profit) as total_profit,  
+                             SUM(total_expenses) as total_expenses_sum  
+                              FROM ( SELECT market, product,
+                                SUM(sales) as sales,
+                                 SUM(profit) as profit,
+                                  SUM(total_expenses) as total_expenses
+                                   FROM business_data
+                                    GROUP BY market, product) AS sub_query
+                                     GROUP BY market;"""
+    data8 = fetch_data(query8)
+    # Most Profitable Markets in Each State       
+    query9 = """SELECT state, MAX(profit) as MaxProfit
+                           FROM ( SELECT state, market, 
+                            SUM(profit) as profit
+                             FROM business_data
+                              GROUP BY state, market) AS sub_query1
+                               GROUP BY state;"""
+    data9 = fetch_data(query9)
+    # Least Profitable Markets in Each State
+    query10 = """SELECT `state`, MIN(`profit`) as `MinProfit`
+                        FROM (
+                         SELECT `state`, `market`, SUM(`profit`) as `profit`
+                          FROM `business_data`
+                           GROUP BY `state`, `market`) AS sub_query1
+                            GROUP BY `state`
+                             ORDER BY `MinProfit` ASC;"""
+    data10 = fetch_data(query10)
+    
+    
+    return render_template("index.html", data1=data1, data2=data2, data3=data3, data4=data4, data5=data5, data6=data6, data7=data7, data8=data8, data9=data9, data10=data10)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ mysql_transform_clean_and_load_data.py
@@ -0,0 +1,91 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+# Connect to MySQL Database
+DATABASE_URL = "mysql+mysqlconnector://root:T9r50wRgf44v@localhost/Sales_Project"
+engine = create_engine(DATABASE_URL)
+
+# Define column data types for SQL database
+from sqlalchemy import Integer, String, Float, Date
+
+sql_dtypes = {
+    'area_code': Integer,
+    'state': String(100),
+    'market': String(100),
+    'market_size': String(100),
+    'profit': Float,
+    'margin': Integer,
+    'sales': Float,
+    'cogs': Integer,
+    'total_expenses': Float,
+    'marketing': Float,
+    'inventory': Integer,
+    'budget_profit': Float,
+    'budget_cogs': Float,
+    'budget_margin': Float,
+    'budget_sales': Float,
+    'productid': Integer,
+    'date': Date,
+    'product_type': String(100),
+    'product': String(100),
+    'type': String(100)
+}
+
+
+try:
+    # 1. Read Excel File
+    print("Reading Excel file...")
+    excel_data = pd.read_excel('sales.xlsx')
+    print("Excel file has been read successfully.")
+
+    # Cleaning column headers
+    excel_data.columns = ["".join(filter(lambda char: char.isprintable(), col)).replace(' ', '_').lower() for col in excel_data.columns]
+    print("Column headers cleaned.")
+
+    # 2. Data Preprocessing
+
+    # Type Validation
+    integer_columns = ['area_code', 'productid']
+    float_columns = ['profit', 'margin', 'sales', 'cogs', 'total_expenses', 'marketing', 'inventory', 'budget_profit', 'budget_cogs', 'budget_sales']
+
+    for col in integer_columns:
+        excel_data[col] = excel_data[col].astype(int, errors='ignore')
+
+    for col in float_columns:
+        excel_data[col] = excel_data[col].astype(float, errors='ignore')
+
+    # Handle Missing Values
+    excel_data[float_columns + integer_columns] = excel_data[float_columns + integer_columns].fillna(0)
+    string_columns = ['state', 'market', 'product_type', 'product', 'type']
+    excel_data[string_columns] = excel_data[string_columns].fillna('Unknown')
+
+    # Date Formatting
+    excel_data['date'] = pd.to_datetime(excel_data['date'], errors='coerce')
+
+    # Text Cleaning
+    excel_data['state'] = excel_data['state'].str.upper()
+    excel_data['market'] = excel_data['market'].str.upper()
+    excel_data['product_type'] = excel_data['product_type'].str.upper()
+    excel_data['product'] = excel_data['product'].str.upper()
+    excel_data['type'] = excel_data['type'].str.upper()
+
+    # Range Checks
+    financial_metrics = ['profit', 'margin', 'sales', 'cogs', 'total_expenses', 'marketing', 'inventory', 'budget_profit', 'budget_cogs', 'budget_sales']
+    excel_data = excel_data[(excel_data[financial_metrics] >= 0).all(axis=1)]
+
+    # Removing Duplicates
+    excel_data.drop_duplicates(inplace=True)
+    print("Data preprocessing completed.")
+
+    # 3. Load data into Database
+    print("Loading data into database...")
+    excel_data.to_sql('Business_Data', engine, dtype=sql_dtypes, index=False, if_exists='replace')
+    print("Data has been loaded successfully!")
+  
+except FileNotFoundError:
+    print("Error: The specified Excel file was not found.")
+except pd.errors.EmptyDataError:
+    print("Error: The Excel file is empty.")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
+
